a5ed26e239cdf758b04847bc0947e1d51ff98c21
ElenaKuropatkina <el-vita81@yandex.ru>
2020-12-30 19:16:03 +0300

    lesson_2 not ready

---
+++ database/src/main/java/com/elenakuropatkina/models/User.java
@@ -0,0 +1,46 @@
+package com.elenakuropatkina.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+
+    private Collection<Role> roles;
+
+    public User(Long id, String login, String firstName) {
+        this.id = id;
+        this.login = login;
+        this.firstName = firstName;
+        //this.lastName = lastName;
+    }
+
+}
